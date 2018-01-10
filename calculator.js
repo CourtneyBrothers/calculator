22d17d2bdb9db5e82006fabf7f061d372ae07672
Courtney Brothers <courtneywbrothers@gmail.com>
2018-01-10 06:16:21 -0600

    add button working

--- calculator.js
+++ calculator.js
@@ -1,3 +1,11 @@
-let field1 = document.getElementById(num1);
-let field2 = document.getElementById(num2);
+let field1 = document.getElementById("num1");
+let field2 = document.getElementById("num2");
+let output = document.getElementById("output")
 
+document.getElementById("btn1").addEventListener("click", ()=>{ 
+    let field1 = parseInt(document.getElementById("num1").value);
+    let field2 = parseInt(document.getElementById("num2").value); 
+    let output = document.getElementById("output");
+
+    output.innerHTML = parseInt(field1+field2);
+});
